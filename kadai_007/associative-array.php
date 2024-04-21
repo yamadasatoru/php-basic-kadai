08a2c258482ea119f94c5676355944599dda77ad
yamadasatoru <75406327+yamadasatoru@users.noreply.github.com>
2024-04-21 14:24:14 +0900

    課題 連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ <head>
+     <meta charset="UTF-8">
+     <title>課題 連想配列を作って値とキーを出力しよう</title>
+ </head>
+
+ <body>
+  <p>
+    <?php
+    // 連想配列に変数を代入する
+    $associative_array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 変数をprint_rで出力する
+    print_r($associative_array);
+    ?>
+  </p>
+ </body>
+
+ </html>
